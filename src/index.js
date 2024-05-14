70a1657a43c244a772fa7736def4486f1beded98
typoscript <minkyu.kim.contact@gmail.com>
2024-05-14 11:47:49 +0900

    Add Chakra Theme

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import theme from "./theme.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider>
-    <App />
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
   </ChakraProvider>
 );
