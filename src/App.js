f04f19b6daef20558db0c3da7de337c0ac541131
typoscript <minkyu.kim.contact@gmail.com>
2024-05-14 16:52:19 +0900

    Add RouterProvider Compoenent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import BookList from "./components/BookList";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 function App() {
   return (
     <>
-      <BookList />
+      <RouterProvider router={router} />
     </>
   );
 }
